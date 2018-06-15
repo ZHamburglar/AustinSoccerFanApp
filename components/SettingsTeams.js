1cac6b12632b0fd79c1fc61a0d93a5405ec6570b
kollinbrandenburg <kollin.brandenburg@oracle.com>
2018-06-14 21:55:44 -0500

    list now producing and functioning checkboxes

---
+++ components/SettingsTeams.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation,
+  TouchableOpacity
+} from 'react-native';
+import { Icon } from 'react-native-elements'
+
+import { connect } from 'react-redux';
+// import * as actions from '../actions';
+
+
+class SettingsTeams extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.easeInEaseOut()
+  }
+
+  state= {
+      checked: false
+  }
+
+  switchState = () => {
+    this.setState({checked: !this.state.checked})
+
+    console.log("switching the state")
+ }
+
+
+  render() {
+    const { id, name } = this.props.leagueteams;
+    console.log('boohoo', this.props.leagueteams)
+
+
+    return (
+    <TouchableOpacity onPress={this.switchState} 
+        style={{
+            height: 100,   
+            width: 100,   
+            backgroundColor: this.state.checked ? 'red' : 'green',   
+            borderRadius: 10,
+            borderWidth: 0.2,
+            borderColor: this.state.checked ? 'red' : 'green'           
+    }}>
+    <Text>
+    {name}
+    </Text>
+    <Icon
+        name={this.state.checked ? 'checkbox-marked' : 'checkbox-blank-outline'}
+        color='#f50'
+        type='material-community'
+        onPress={() => console.log('hello')} />
+
+    </TouchableOpacity>
+
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  },
+  descriptionStyle: {
+    paddingLeft: 10,
+    paddingRight: 10
+  }
+};
+
+// const mapStateToProps = (state, ownProps) => {
+//   const expanded = state.selectedServerId === ownProps.server.id
+//   return { expanded };
+// };
+
+export default connect(null, null)(SettingsTeams);
--- screens/SupportersGroups.js
+++ screens/SupportersGroups.js
@@ -5,13 +5,10 @@ import { createStackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 
 import Expo from 'expo';
-import KeysButtons from '../components/KeysButtons';
-import CapoButtons from '../components/CapoButtons';
 import CapoKey from '../components/CapoKey';
 import BottomNavBar from '../components/BottomNavBar';
+import SettingsTeams from '../components/SettingsTeams';
 
-import ChordsModal from '../modals/ChordsModal';
-import ViewChordsButton from '../components/ViewChordsButton';
 import icon from '../assets/icons/pure-icon.png';
 import { STATUS_BAR_HEIGHT, SCREEN_WIDTH } from '../constants';
 
@@ -21,7 +18,17 @@ const cacheImages = images => images.map(image => {
 });
 
 class SupportersGroups extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
+    this.dataSource = ds.cloneWithRows(this.props.leagueteams.MLS);
+  }
+
+  renderRow(leagueteams) {
+    return <SettingsTeams leagueteams={leagueteams} />;
+  }
 
   state = {
     appIsReady: false,
@@ -36,177 +43,16 @@ class SupportersGroups extends Component {
 
 
   render() {
-    console.log("props", this.props.leagueteams)
+    console.log("props", this.props.leagueteams.MLS, "EPL", this.props.leagueteams.EPL)
     const { leagueteams } = this.props.leagueteams
 
     const { containerStyle, dividerStyle, buttonContainerStyle } = styles;
 
     return (
-      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
-          <ScrollView>
-          
-            
-            <Text>MLS</Text>
-            <CheckBox
-              title='Atlanta United'
-              checked={this.state.checked}
-              onPress={this.logtheprops}
-            />
-            <CheckBox
-              title='Chicago Fire'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Colorado Rapids'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Columbus Crew SC'
-              checked={this.state.checked}
-              onPress={() => console.log("hello")}
-            />
-            <CheckBox
-              title='DC United'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='FC Dallas'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Houston Dynamo'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='LA Galaxy'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='LAFC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Minnesota United'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Montreal Impact'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='New England Revolution'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='NYCFC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='New York Red Bulls'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Orlando City FC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Philadelphia Union'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Portland Timbers'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Real Salt Lake'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='San Jose Earthquakes'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Seattle Sounders FC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Sporting KC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Toronto FC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            <CheckBox
-              title='Vancouver Whitecaps FC'
-              checked={this.state.checked}
-              onPress={() => this.setState({
-                checked: !this.state.checked
-              })}
-            />
-            
-          </ScrollView>
-      </View>
+      <ListView
+      dataSource={this.dataSource}
+      renderRow={this.renderRow}
+    />
     );
   }
 }
